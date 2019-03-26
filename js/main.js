6ec5524898bb314ffacdff31e37784a284abadbc
Toxic <nhirci@gmail.com>
2019-03-26 23:30:46 +0100

    rebuilding site Tue, Mar 26, 2019 11:30:46 PM

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-    if ( $( 'html' ).hasClass( 'fp-enabled' ) ) {
-        $.fn.fullpage.destroy('all');
-    }
     $('#fullpage').fullpage({
         //options here
         navigation: true,
